32dab1e12db19564cee24801ea8d4b42b8429ad1
Sohmteee <sohmtee@gmail.com>
2022-08-08 22:04:36 +0100

    commit

--- src/IntegerToRoman/Main.java
+++ src/IntegerToRoman/Main.java
@@ -11,6 +11,13 @@ public class Main {
                 hundreds = new HashMap<>(),
                 thousands = new HashMap<>();
 
+        String [] unit = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String [] ten = {"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String [] hundred = {"C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String [] thousand = {"M", "MM", "MMM"};
+
+        for (int i = 1; i <= unit.length; i++) units.put(i, unit[i]);
+
         return roman;
     }
 }
