5ac9db24ed81ea1182875fea3021acbacdeb9e49
Sohmteee <sohmtee@gmail.com>
2022-08-08 21:58:15 +0100

    commit

--- src/IntegerToRoman/Main.java
+++ src/IntegerToRoman/Main.java
@@ -1,5 +1,16 @@
 package IntegerToRoman;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    
+    public String intToRoman(int num) {
+        String roman = "";
+        Map<Integer, String> units = new HashMap<>(),
+                tens = new HashMap<>(),
+                hundreds = new HashMap<>(),
+                thousands = new HashMap<>();
+
+        return roman;
+    }
 }
