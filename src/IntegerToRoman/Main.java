8e4ddfdb879fe54e1166ea5ec5e74755915b28b4
Sohmteee <sohmtee@gmail.com>
2022-08-08 22:53:54 +0100

    commit

--- src/IntegerToRoman/Main.java
+++ src/IntegerToRoman/Main.java
@@ -4,6 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+
+    public static Map<Integer, String> units = new HashMap<>(),
+            tens = new HashMap<>(),
+            hundreds = new HashMap<>(),
+            thousands = new HashMap<>();
     public static String[] unit = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
     public static String[] ten = {"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
     public static String[] hundred = {"C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
@@ -12,10 +17,6 @@ public class Main {
     public static String intToRoman(int num) {
         String roman = "";
         if (num >= 4000) return "";
-        Map<Integer, String> units = new HashMap<>(),
-                tens = new HashMap<>(),
-                hundreds = new HashMap<>(),
-                thousands = new HashMap<>();
 
         for (int i = 1; i <= unit.length; i++) units.put(i, unit[i - 1]);
         for (int i = 1; i <= ten.length; i++) tens.put(i, ten[i - 1]);
@@ -25,6 +26,7 @@ public class Main {
         int count = 1;
         while (num > 0) {
             int digit = num % 10;
+            var letter = getPlaceValue(count);
             roman =
         }
 
@@ -34,15 +36,19 @@ public class Main {
                 (units.get(num) == null ? "" : units.get(num));
     }
 
-    public static String[] getPlacevalue(int num) {
-        switch(num) {
-            case 1 : return unit;
-            case 2 : return ten;
-            case 3 : return hundred;
-            case 4 : return thousand;
-            default: return new String[] {};
+    public static String[] getPlaceValue(int num) {
+        switch (num) {
+            case 1:
+                return unit;
+            case 2:
+                return ten;
+            case 3:
+                return hundred;
+            case 4:
+                return thousand;
+            default:
+                return new String[]{};
         }
-        
     }
 
     public static void main(String[] args) {
