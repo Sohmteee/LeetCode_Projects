2318453f2204c2834ee6a44225b240d5569bb5a1
Sohmteee <sohmtee@gmail.com>
2022-08-09 01:19:29 +0100

    commit

--- src/RomanToInteger/Solution.java
+++ src/RomanToInteger/Solution.java
@@ -2,26 +2,26 @@ package RomanToInteger;
 
 public class Solution {
     public static int romanToInt(String s) {
-        s.replace("IV", "4");
-        s.replace("IX", "9");
-        s.replace("XL", "4");
-        s.replace("XC", "9");
-        s.replace("CD", "4");
-        s.replace("CM", "9");
+        s = s.replace("IV", "4");
+        s = s.replace("IX", "9");
+        s = s.replace("XL", "4");
+        s = s.replace("XC", "9");
+        s = s.replace("CD", "4");
+        s = s.replace("CM", "9");
 
-        s.replace("I", "1");
-        s.replace("V", "5");
-        s.replace("X", "1");
-        s.replace("L", "5");
-        s.replace("C", "1");
-        s.replace("D", "5");
-        s.replace("M", "1");
+        s = s.replace("I", "1");
+        s = s.replace("V", "5");
+        s = s.replace("X", "1");
+        s = s.replace("L", "5");
+        s = s.replace("C", "1");
+        s = s.replace("D", "5");
+        s = s.replace("M", "1");
 
         return Integer.parseInt(s);
 
     }
 
     public static void main(String[] args) {
-        System.out.println(romanToInt("I"));
+        System.out.println(romanToInt("IV"));
     }
 }
