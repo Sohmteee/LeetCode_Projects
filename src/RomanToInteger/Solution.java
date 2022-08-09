5192032e2809f137734989f3ad4c536d042e53f6
Sohmteee <sohmtee@gmail.com>
2022-08-09 23:22:30 +0100

    commit

--- src/RomanToInteger/Solution.java
+++ src/RomanToInteger/Solution.java
@@ -49,6 +49,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(romanToInt("lviii"));
+        System.out.println(romanToInt("LVIII"));
     }
 }
