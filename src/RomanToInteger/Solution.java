0056e37747e4c646c59c5a9e2a8ad6211dcb8801
Sohmteee <sohmtee@gmail.com>
2022-08-09 01:13:06 +0100

    commit

--- src/RomanToInteger/Solution.java
+++ src/RomanToInteger/Solution.java
@@ -1,9 +1,16 @@
 package RomanToInteger;
 
 public class Solution {
-
+    public static int romanToInt(String s) {
+        s.replace("IV", "4");
+        s.replace("IX", "9");
+        s.replace("XL", "40");
+        s.replace("XC", "90");
+        s.replace("IV", "4");
+        s.replace("IV", "4");
+    }
 
     public static void main(String[] args) {
-        
+        System.out.println(romanToInt("IV"));
     }
 }
