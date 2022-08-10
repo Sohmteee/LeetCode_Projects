dc8237500552d7ca52c7796867d8c8e592ccaf33
Sohmteee <sohmtee@gmail.com>
2022-08-10 02:42:13 +0100

    commit

--- src/RomanToInteger/Solution.java
+++ src/RomanToInteger/Solution.java
@@ -28,26 +28,11 @@ public class Solution {
         rom = getString(s, thousands)+
                 getString(s, hundreds) +
                 getString(s, tens) +
-                getString(s, units) + " ";
+                getString(s, units);
 
         System.out.println(rom);
 
-        /*s = s.replace("IV", "4");
-        s = s.replace("IX", "9");
-        s = s.replace("XL", "4");
-        s = s.replace("XC", "9");
-        s = s.replace("CD", "4");
-        s = s.replace("CM", "9");
-
-        s = s.replace("I", "1");
-        s = s.replace("V", "5");
-        s = s.replace("X", "1");
-        s = s.replace("L", "5");
-        s = s.replace("C", "1");
-        s = s.replace("D", "5");
-        s = s.replace("M", "1");*/
-
-        return Integer.parseInt(s);
+        return Integer.parseInt(rom);
 
     }
 
