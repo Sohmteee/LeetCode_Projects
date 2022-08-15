0a93c07ff3036daf1264f963ed6c81f0acdfb5b0
Sohmteee <sohmtee@gmail.com>
2022-08-15 21:09:25 +0100

    commit

--- src/PascalsTriangle/Main.java
+++ src/PascalsTriangle/Main.java
@@ -5,7 +5,8 @@ Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's
 
 In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
-  
+          1
+        
 Example 1:
 
 Input: rowIndex = 3
